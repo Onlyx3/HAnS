ab6eb6d9ab4096b1d6dfc6d2ac18a27914f3e238
Onlyx3 <onlyx3osu@gmail.com>
2025-08-11 14:22:04 +0200

    Added Utility class to find PsiElement at specified location

---
+++ src/main/java/featracer/util/Utility.java
@@ -0,0 +1,48 @@
+package featracer.util;
+
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.*;
+import com.intellij.psi.search.FilenameIndex;
+import com.intellij.psi.search.GlobalSearchScope;
+
+public class Utility {
+
+
+    /**
+     *
+     * @param project
+     * @param location String in the format "classname:codeline"
+     * @return PsiElement with specified location, null if not found
+     */
+    public static PsiElement findPsiElementFromLocation(Project project, String location) {
+        // Format Input
+        String[] split = location.split(":");
+        if (split.length != 2) return null;
+
+        String className = split[0];
+        int lineNumber = Integer.parseInt(split[1]);
+        if (lineNumber < 1) return null;
+
+
+        JavaPsiFacade psiFacade = JavaPsiFacade.getInstance(project);
+        GlobalSearchScope searchScope = GlobalSearchScope.projectScope(project);
+        PsiClass psiClass = psiFacade.findClass(className, searchScope);
+
+        if (psiClass == null || psiClass.getContainingFile() == null) return null;
+        PsiFile psiFile = psiClass.getContainingFile();
+
+        Document document = PsiDocumentManager.getInstance(project).getDocument(psiFile);
+        if (document == null) return null;
+
+        if (lineNumber > document.getLineCount()) return null;
+
+        return psiFile.findElementAt(document.getLineStartOffset(lineNumber - 1));
+
+
+       /* GlobalSearchScope searchScope = GlobalSearchScope.allScope(project);
+        PsiFile[] psiFile = FilenameIndex.getFilesByName(project, className, searchScope); // find non deprecated way to do this
+
+        if (psiFile.length == 0) return null;*/
+    }
+}
