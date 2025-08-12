95687b47a31b7167adba328c9d3f8c78ab23c9e9
Onlyx3 <onlyx3osu@gmail.com>
2025-08-12 13:20:08 +0200

    Fixed Issue with reading access in findPsiElementFromLocation

--- src/main/java/featracer/util/Utility.java
+++ src/main/java/featracer/util/Utility.java
@@ -1,7 +1,9 @@
 package featracer.util;
 
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Computable;
 import com.intellij.psi.*;
 import com.intellij.psi.search.FilenameIndex;
 import com.intellij.psi.search.GlobalSearchScope;
@@ -12,7 +14,7 @@ public class Utility {
     /**
      *
      * @param project
-     * @param location String in the format "classname:codeline"
+     * @param location String in the format "package.classname:codeline"
      * @return PsiElement with specified location, null if not found
      */
     public static PsiElement findPsiElementFromLocation(Project project, String location) {
@@ -23,23 +25,24 @@ public class Utility {
         String className = split[0];
         int lineNumber = Integer.parseInt(split[1]);
         if (lineNumber < 1) return null;
+        return ApplicationManager.getApplication().runReadAction(
+                (Computable<PsiElement>) () -> {
+                    JavaPsiFacade psiFacade = JavaPsiFacade.getInstance(project);
+                    GlobalSearchScope searchScope = GlobalSearchScope.projectScope(project);
+                    PsiClass psiClass = psiFacade.findClass(className, searchScope);
 
+                    if (psiClass == null || psiClass.getContainingFile() == null) return null;
+                    PsiFile psiFile = psiClass.getContainingFile();
 
-        JavaPsiFacade psiFacade = JavaPsiFacade.getInstance(project);
-        GlobalSearchScope searchScope = GlobalSearchScope.projectScope(project);
-        PsiClass psiClass = psiFacade.findClass(className, searchScope);
+                    Document document = PsiDocumentManager.getInstance(project).getDocument(psiFile);
+                    if (document == null) return null;
 
-        if (psiClass == null || psiClass.getContainingFile() == null) return null;
-        PsiFile psiFile = psiClass.getContainingFile();
+                    if (lineNumber > document.getLineCount()) return null;
 
-        Document document = PsiDocumentManager.getInstance(project).getDocument(psiFile);
-        if (document == null) return null;
+                    return psiFile.findElementAt(document.getLineStartOffset(lineNumber - 1));
 
-        if (lineNumber > document.getLineCount()) return null;
-
-        return psiFile.findElementAt(document.getLineStartOffset(lineNumber - 1));
-
-
+                }
+        );
        /* GlobalSearchScope searchScope = GlobalSearchScope.allScope(project);
         PsiFile[] psiFile = FilenameIndex.getFilesByName(project, className, searchScope); // find non deprecated way to do this
 
