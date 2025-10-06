3ce8a0616ad037d5a98a61e6c5d91e417577a6b8
Onlyx3 <onlyx3osu@gmail.com>
2025-10-07 00:19:30 +0200

    Test changes

--- src/main/java/featracer/test/FeatRacerTest.java
+++ src/main/java/featracer/test/FeatRacerTest.java
@@ -5,13 +5,23 @@ import com.intellij.notification.NotificationGroupManager;
 import com.intellij.notification.NotificationType;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.application.PathManager;
+import com.intellij.openapi.progress.ProgressIndicator;
+import com.intellij.openapi.progress.ProgressManager;
+import com.intellij.openapi.progress.Task;
 import com.intellij.openapi.project.Project;
+import featracer.data.FeatRacerStateService;
 import featracer.data.RecommendationData;
+import featracer.logic.ClassifierManager;
 import featracer.util.Utility;
 import org.jetbrains.annotations.NotNull;
+import se.gu.api.FeatRacerAPI;
 
+import java.io.File;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class FeatRacerTest {
 
@@ -47,31 +57,53 @@ public class FeatRacerTest {
     //    RecommendationData testLine = Utility.makeRecommendationData(project, "featracer.test.FeatRacerTest:37", testfeatures);
     //    RecommendationData outOfBounds = Utility.makeRecommendationData(project, "featracer.test.FeatRacerTest:2222",  testfeatures);
     //    RecommendationData classpathinvalid = Utility.makeRecommendationData(project, "thisdoesntexist:2", testfeatures);
-        RecommendationData testBlock = Utility.translateFeatracerLocation(project, "test/testing/FeatRacerTest.java::67-68", testfeatures);
-        RecommendationData testLine = Utility.translateFeatracerLocation(project, "ihopethisisirrelevant/FeatRacerTest.java::70", testfeatures);
+        RecommendationData testBlock = Utility.translateFeatracerLocation(project, "test/testing/FeatRacerTest.java::60-109", testfeatures);
+        RecommendationData testLine = Utility.translateFeatracerLocation(project, "ihopethisisirrelevant/FeatRacerTest.java::64-64", testfeatures);
 
 
         List<RecommendationData> list = new ArrayList<>();
-        list.add(testLine);
         list.add(testBlock);
+        list.add(testLine);
     //    list.add(outOfBounds);
     //    list.add(classpathinvalid);
      //   list.add(wrongFormat);
 
         Utility.checkAndInvokeRecommendationWizard(project, list);
     }
+/*
+    public static void runTest(@NotNull Project project) {
+        int startingCommit = 1;
+        String projectPath = project.getBasePath();
+        String analysisPath = Paths.get(PathManager.getSystemPath(), "featracer-analysis", project.getName()).toString();
+        ProgressManager.getInstance().run(new Task.Backgroundable(project, "Running FeatRacer..."){
+            List<RecommendationData> recommendations = new ArrayList<>();
 
+            public void run(@NotNull ProgressIndicator indicator) {
+                indicator.setFraction(0.0);
 
-    private void testFunction() {
+                String projectGitPath = Paths.get(projectPath, ".git").toString();
+                File gitDir = new File(projectGitPath);
+                if(!gitDir.exists() || !gitDir.isDirectory()){
+                    throw new RuntimeException("Project Git directory does not exist or is not a directory");
+                }
 
-        int x = 1337;
-        int y = 42;
+                FeatRacerAPI featRacerAPI = new FeatRacerAPI();
+                Map<String, List<String>> result;
+                try {
+                    result = featRacerAPI.testRecommendationAll(projectGitPath, startingCommit, analysisPath, ".js,.c,.cpp,.h,.cc,.y,.py,.java");
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
 
-        process();
+                for (Map.Entry<String, List<String>> entry : result.entrySet()) {
+                    RecommendationData rec = Utility.translateFeatracerLocation(project, entry.getKey(), entry.getValue());
+                    if (rec != null) recommendations.add(rec);
+                }
 
-    }
-
-    private void process() {
-        //do nothing
-    }
+            }
+            public void onSuccess() {
+                Utility.checkAndInvokeRecommendationWizard(project, recommendations);
+            }
+        });
+    }*/
 }
